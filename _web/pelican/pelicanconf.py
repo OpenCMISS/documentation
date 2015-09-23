2fa6b8078360e5475754041ba335df41e0048ed1
Noel Zeng @ ABI <abinoel@localhost.localdomain>
2015-09-23 17:57:19 +1200

    add news index page

--- _web/pelican/pelicanconf.py
+++ _web/pelican/pelicanconf.py
@@ -38,7 +38,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 3
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
