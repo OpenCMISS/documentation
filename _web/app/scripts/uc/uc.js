621c33151c2ba868ba8e6d9cbb46f406d3a8c337
Noel Zeng @ ABI <hello@noel.geek.nz>
2015-10-14 19:08:21 +1300

    fix under construction script to run on distribution build

--- _web/app/scripts/uc/uc.js
+++ _web/app/scripts/uc/uc.js
@@ -4,14 +4,17 @@
 
 (function(){
 	$(document).ready(function(){
-		var ucLinks = $('a[href="/underconstruction.html"]');
+		var ucRegExp = new RegExp('/underconstruction.html');
+		var ucLinks = $('a').filter(function(i,anchor){
+			return ucRegExp.test($(anchor).attr('href'))
+		});
 		console.log("Debug: found "+ucLinks.length+" under construction page(s).");
 		ucLinks.each(function(i,link){
 			var $link = $(link);
 			$link.attr('title','This page is still under construction.');
 			var $linkText  = $(link).find('p');
 			if ($linkText.length > 0){
-				$($linkText[0]).append('<span class="glyphicon glyphicon-thumbs-down" title="This page is still under construction." style="font-size:12px;padding-left:5px;"></span>');
+				$($linkText[0]).append('<span class="glyphicon glyphicon-thumbs-down" style="font-size:12px;padding-left:5px;"></span>');
 			}
 		});
 	});
